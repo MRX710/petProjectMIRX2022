2bb8ffcf2d0373a273ee4b4214c51c9b4202d0e9
MIRX001 <alexnik.01@mail.ru>
2022-10-07 01:45:12 +0300

    structure of project is begin to building

---
+++ src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -0,0 +1,30 @@
+import React, {useMemo, useState} from 'react';
+import {LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext} from '../lib/ThemeContext';
+import {IChildren} from "types/mainPropsTypes";
+
+
+const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme || Theme.LIGHT;
+
+interface IThemeProvider extends IChildren{
+
+}
+
+const ThemeProvider: React.FC<IThemeProvider> = ({children}) => {
+
+   const [theme, setTheme] = useState<Theme>(defaultTheme)
+
+   const defaultProps = useMemo(() => ({
+      theme: theme,
+      setTheme: setTheme
+   }), [theme])
+
+   return (
+       <ThemeContext.Provider
+         value={defaultProps}
+       >
+          {children}
+       </ThemeContext.Provider>
+   );
+};
+
+export default ThemeProvider;
---
+++ src/types/mainPropsTypes.ts
@@ -0,0 +1,8 @@
+import React, {CSSProperties, ReactNode} from "react";
+
+export interface IChildren{
+   children: ReactNode
+}
+export interface IStyle{
+   children: CSSProperties
+}

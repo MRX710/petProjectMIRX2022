25e632f8ac69e7df7b3f769e4ed3882ffb6174eb
MIRX001 <alexnik.01@mail.ru>
2023-04-02 22:15:20 +0300

    404 page

---
+++ src/widgets/PageLoader/ui/PageLoader.tsx
@@ -0,0 +1,19 @@
+import { FC } from 'react';
+import './PageLoader.scss';
+import { classNames } from 'shared/lib/classNames/classNames';
+import {Loader} from "shared/ui/Loader/Loader";
+
+interface PageLoaderProps {
+   className?: string
+}
+
+export const PageLoader: FC<PageLoaderProps> = (props) => {
+   const {className, children, ...otherProps} = props;
+
+   return (
+       <div className={classNames('page-loader', {}, [className])} >
+          <Loader/>
+       </div>
+   );
+};
+

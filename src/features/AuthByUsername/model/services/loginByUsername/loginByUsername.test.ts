8d761cbece634031a85997e98f25da000ce990b6
MIRX001 <alexnik.01@mail.ru>
2023-05-05 00:55:17 +0300

    fix files after turn on strict mode#2

--- src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.test.ts
+++ src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.test.ts
@@ -3,10 +3,6 @@ import { userActions } from 'entities/User';
 import { TestAsyncThunk } from 'shared/lib/tests/TestAsyncThunk/TestAsyncThunk';
 import { loginByUsername } from './loginByUsername';
 
-jest.mock('axios');
-
-const mockedAxios = jest.mocked(axios, true);
-
 
 describe('loginByUsername', () => {
     // let dispatch: Dispatch;
@@ -47,28 +43,28 @@ describe('loginByUsername', () => {
 
     test('success', async () => {
         const userValue = { username: '123', id: '1' };
-        mockedAxios.post.mockReturnValue(
-            Promise.resolve({ data: userValue }),
-        );
 
         const thunk = new TestAsyncThunk(loginByUsername);
+        thunk.api.post.mockReturnValue(
+            Promise.resolve({ data: userValue }),
+        );
         const result = await thunk.callThunk({ username: '123', password: '123' });
 
         expect(thunk.dispatch).toHaveBeenCalledWith(userActions.setAuthData(userValue));
         expect(thunk.dispatch).toHaveBeenCalledTimes(3);
-        expect(mockedAxios.post).toHaveBeenCalled();
+        expect(thunk.api.post).toHaveBeenCalled();
         expect(result.meta.requestStatus).toBe('fulfilled');
     });
 
 
     test('rejected', async () => {
-        mockedAxios.post.mockReturnValue(
+        const thunk = new TestAsyncThunk(loginByUsername);
+        thunk.api.post.mockReturnValue(
             Promise.resolve({ status: 403 }),
         );
-        const thunk = new TestAsyncThunk(loginByUsername);
         const result = await thunk.callThunk({ username: '123', password: '123' });
 
-        expect(mockedAxios.post).toHaveBeenCalled();
+        expect(thunk.api.post).toHaveBeenCalled();
         expect(thunk.dispatch).toHaveBeenCalledTimes(2);
         expect(result.meta.requestStatus).toBe('rejected');
         expect(result.payload).toBe('error');

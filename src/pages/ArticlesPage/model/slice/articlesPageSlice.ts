617a2e0f5430cf0ae0720a6db29ac022b060b1dd
MIRX001 <alexnik.01@mail.ru>
2023-05-24 14:48:10 +0300

    added list of articles in two views #4

--- src/pages/ArticlesPage/model/slice/articlesPageSlice.ts
+++ src/pages/ArticlesPage/model/slice/articlesPageSlice.ts
@@ -1,12 +1,28 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createEntityAdapter, createSlice } from '@reduxjs/toolkit';
+import { IArticleDetailsCommentsScheme } from "pages/ArticleDetailPage";
+import { IComment } from "entities/Comment";
+import { StateScheme } from "app/providers/StoreProvider";
 import { IArticlesPageScheme } from '../types/articlesPageScheme';
 
+const initialState: IArticlesPageScheme = {
+    isLoading: false,
+    error: undefined,
+    ids: [],
+    entities: {},
+};
 
-const initialState: IArticlesPageScheme = {};
+const articlesPageAdapter = createEntityAdapter<IComment>({
+    selectId: (comment) => comment.id,
+});
+
+export const getArticles = articlesPageAdapter.getSelectors<StateScheme>(
+    (state) => state?.articleDetailsComments || articlesPageAdapter.getInitialState(),
+);
+
 
 export const articlesPageSlice = createSlice({
-    name: 'articles',
-    initialState,
+    name: 'articlesPage',
+    initialState: articlesPageAdapter.getInitialState<IArticlesPageScheme>(initialState),
     reducers: {},
 });
 export const { actions: articlesPageActions } = articlesPageSlice;

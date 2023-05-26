01de7cda82458a9049f67a3d2f84722aa583a146
MIRX001 <alexnik.01@mail.ru>
2023-05-26 18:06:25 +0300

    fix bug of infinite scroll

--- src/shared/lib/hooks/useInfiniteScroll/useInfiniteScroll.ts
+++ src/shared/lib/hooks/useInfiniteScroll/useInfiniteScroll.ts
@@ -8,10 +8,12 @@ export interface UseInfiniteScrollProps {
 
 export function useInfiniteScroll({ callback, triggerRef, wrapperRef }: UseInfiniteScrollProps) {
     useEffect(() => {
+        const wrapperElement = wrapperRef!.current;
+        const triggerElement = triggerRef!.current;
         let observer: IntersectionObserver | null = null;
         if (callback) {
             const options = {
-                root: wrapperRef.current,
+                root: wrapperElement,
                 rootMargin: "0px",
                 threshold: 1.0,
             };
@@ -22,12 +24,12 @@ export function useInfiniteScroll({ callback, triggerRef, wrapperRef }: UseInfin
                 }
             }, options);
 
-            observer.observe(triggerRef.current);
+            observer.observe(triggerElement);
         }
         return () => {
             if (observer) {
             // eslint-disable-next-line react-hooks/exhaustive-deps
-                observer.unobserve(triggerRef.current);
+                observer.unobserve(triggerElement);
             }
         };
     }, [callback, triggerRef, wrapperRef]);
